39e1cd3838455dae3fd83cd607a5a1c32afdc411
İkbal Yaşar <miyasar@gtu.edu.tr>
2018-12-07 15:10:34 +0300

    add tabbar

--- AfistReact/App.js
+++ AfistReact/App.js
@@ -8,7 +8,8 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import {TabNavigator} from 'react-navigation'; 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,34 +18,102 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+class AkifeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Cennet!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Akife</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+class FurkanScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Furkan</Text>
+      </View>
+    );
+  }
+}
+
+class IkbalScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>İkbal</Text>
+      </View>
+    );
+  }
+}
+
+class SalihScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Salih</Text>
+      </View>
+    );
+  }
+}
+
+class TeomanScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Teoman</Text>
+      </View>
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Akife: AkifeScreen,
+  Furkan: FurkanScreen,
+  Ikbal: IkbalScreen,
+  Salih: SalihScreen,
+  Teoman: TeomanScreen
+
+},
+{
+  defaultNavigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, horizontal, tintColor }) => {
+      const { routeName } = navigation.state;
+      let iconName;
+      if (routeName === 'Akife') {
+        iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+      } else if (routeName === 'Furkan') {
+        iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+      }
+      else if (routeName === 'Ikbal') {
+        if(focused) {
+          iconName = `ios-information-circle`;
+
+        }
+        else {
+          iconName = `md-notifications-outline`;
+        }
+        
+      }
+      else if (routeName === 'Salih') {
+        iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+      }
+      else if (routeName === 'Teoman') {
+        iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+      }
+
+      // You can return any component that you like here! We usually use an
+      // icon component from react-native-vector-icons
+      return <Ionicons name={iconName} size={horizontal ? 20 : 25} color={tintColor} />;
+    },
+  }),
+  tabBarOptions: {
+    activeTintColor: 'blue',
+    inactiveTintColor: 'gray',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+}
+);
+
+export default createAppContainer(TabNavigator);
+
